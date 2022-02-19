fa3eff8b452e86c976f229fbbabd547198dc2a02
Aleksandr Nariadchikov <alexnar1771@gmail.com>
2022-02-19 12:28:13 +0100

    Adding simple materials
    - Dissolve material;
    - Health bar material;
    - Hit material;

---
+++ Source/ActionRoguelike/Private/Demo/STargetDummy.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Demo/STargetDummy.h"
+
+#include "Components/SAttributeComponent.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	// Set up static mesh component
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+
+	// Set up attribute component
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+}
+
+void ASTargetDummy::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	AttributeComp->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewValue,
+                                    float Delta)
+{
+	if (Delta < 0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials(HitMaterialParameterName, GetWorld()->GetTimeSeconds());
+	}
+}
---
+++ Source/ActionRoguelike/Public/Demo/STargetDummy.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STargetDummy.generated.h"
+
+class USAttributeComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASTargetDummy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASTargetDummy();
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	USAttributeComponent* AttributeComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Config")
+	FName HitMaterialParameterName;
+
+	// Allow actors to initialize themselves on the C++ side after all of their components have been initialized
+	virtual void PostInitializeComponents() override;
+	
+	UFUNCTION()
+	void OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewValue, float Delta);
+};
